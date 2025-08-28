f8859556271e4696e2f714b51803d5abc539c35e
boanergaes <mebitzeamanuel@gmail.com>
2025-08-29 01:16:10 +0300

    installed mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const { watchFile } = require('fs')
 
 module.exports = {
@@ -34,7 +35,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
 
             {
@@ -60,6 +61,7 @@ module.exports = {
             title: 'Tic-Tac-Toe',
             filename: 'index.html',
             template: 'src/pages/home.html',
-        })
+        }),
+        new MiniCssExtractPlugin()
     ]
 }
